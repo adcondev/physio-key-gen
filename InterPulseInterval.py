2ac3203d0748db1a6ac69bcf389205c1d9883aa5
SenoReload <secante.cosecante@gmail.com>
2020-04-26 21:25:23 -0500

    Cálculo de IPI

---
+++ InterPulseInterval.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import shutil
+import wfdb
+def IPI(filter, filename, start, step, threshold):
+	"""Función para determinar el IPI de un segmento de tiempo."""
+	record = wfdb.rdrecord(filename)
+	fs = record.__dict__["fs"]
+	begin = start*fs
+	end = int(step*fs)
+	# encontrar pico dado un umbral
+	record.__dict__['p_signal'] = record.__dict__['p_signal'][begin:begin + end]
+	ecg_signal = np.array([lista[0] for lista in record.__dict__['p_signal']])
+	# Vector lineal de función senoidal de 0.5 pi a 1.5 pi con n muestras(filtro)
+	t = np.linspace(0.5 * np.pi, 1.5 * np.pi, filter)
+	# Usar la función senoidal para determinar el tiempo entre pulsos
+	qrs_filter = np.sin(t)
+	# Computa cross correlation entre el ECG y el filtro propuesto
+	similarity = np.correlate(ecg_signal, qrs_filter, mode="same")
+	for i in range(end):
+		record.__dict__['p_signal'][i][1] = similarity[i]
+	peaks = np.nonzero(similarity > threshold)
+	# IPI es el promedio de las diferencias
+	peaks_DxDt = np.dot(np.diff(peaks), 1/fs)
+	IPI = np.average(peaks_DxDt[peaks_DxDt > 1/fs])
+	# Gráficas
+	wfdb.plot_wfdb(record=record, title='Record ' + filename[-3:] + ' from MIT-BIH Arrhythmia DB')
+	return IPI, record
+if __name__ == '__main__':
+	ipi,record = IPI(5, 'physionet2/103', 94, 2.5, 2)
+	print("IPI Value:", ipi)
